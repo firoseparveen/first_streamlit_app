7752458918c6412662086f7c6ecb2a0890aef496
firoseparveen <159011756+firoseparveen@users.noreply.github.com>
2024-02-08 17:15:40 +0530

    Update setup.py
    upgrade pip

--- setup.py
+++ setup.py
@@ -1,5 +1,2 @@
 # Upgrade pip
 os.system("/home/adminuser/venv/bin/python -m pip install --upgrade pip")
-
-# Install scikit-learn
-os.system("/home/adminuser/venv/bin/python -m pip install scikit-learn")
